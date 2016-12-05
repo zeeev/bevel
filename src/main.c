7d109a658ec30ca32c34213e230b0aeda0dc0248
zev kronenberg <jewbaru@gmail.com>
2016-12-05 10:50:03 -0800

    global struct now

--- src/main.c
+++ src/main.c
@@ -34,10 +34,13 @@ unsigned char seq_nt4_table[256] = {
 
 static inline print_mr(struct mr * m){
 	printf("minimizer:\n");
-	printf(" min: %" PRId64 "\n", m->min);
-	printf(" strand: %i\n", m->load & 1);
-	printf(" rid: %i\n", (m->load >> 32));
-	printf(" pos: %i\n", (m->load << 32));
+	printf(" min: %" PRIu64    "\n",  m->min);
+	printf(" load: %" PRIu64   "\n",  m->load );
+	printf(" strand: %" PRIu64 "\n",  m->load & 1);
+	printf(" rid: %" PRIu64    "\n",  m->load>>32 );
+	printf(" pos: %i"     "\n",  (uint32_t)m->load>>1 );
+
+
 	printf("\n");
 }
 
@@ -64,69 +67,77 @@ static inline uint64_t hash64(uint64_t key, uint64_t mask)
  */
 void sketch(const char * name, const char * seq,
 									int len, int k, int w,
-									uint32_t rid){
+									uint32_t rid,
+									struct mr ** data,
+									int  * datumSize ){
 
   uint64_t shift1 = 2 * (k - 1), mask = (1ULL << 2*k)-1;
 
 	struct kp kmers;
+	kmers.km[0] = 0; kmers.km[1] = 0;
 
 	uint64_t wa = 0, cr = 0, min = 0;
 	uint8_t strand = 0;
 
-	kmers.km[0] = 0; kmers.km[1] = 0;
-
   assert(len > 0 && w > 0 && k > 0 && len > k && len > w && k < 64/2);
 
-  int i = 0, l = 0, n = 0;
-	int s = (int)len/(float)w ;
+	int s = ((int)len/(float)w) +1 ;
+
+	// $i is the base index
+	// $j is the count of acceptable bases
+	// $l last kmer window
+	// $n buffer index
+  int i = 0, j = 0, l = 0, n = 0;
+
+	l = k;
 
 	struct mr * buffer = (struct mr *) malloc( sizeof(struct mr) * s);
 
-	for(;i < s; i++){
+	for(i = 0;i < s; i++){
 		buffer[i].min     = UINT64_MAX;
 		buffer[i].load    = 0         ;
 	}
 
-	i = 0;
-
-  for(; i < len; i++){
+  for(i = 0; i < len; i++){
 		int c = seq_nt4_table[(uint8_t)seq[i]];
 		if(c < 4){
 			kmers.km[0] = (kmers.km[0] << 2  | c ) & mask        ;
 			kmers.km[1] = (kmers.km[1] >> 2) | (3ULL^c) << shift1;
+			j++;
+		}
+		if(j >= k && i >= l){
 			if(kmers.km[0] == kmers.km[1]) continue;
-			l++;
-		}
-		else{
-			l = 0;
-		}
-		if((l % w) == 0 && l > k){
-			n++;
-		}
-		if(l >= k){
-			assert(n < s);
 			wa = hash64(kmers.km[0], mask);
 			cr = hash64(kmers.km[1], mask);
 			strand = wa > cr ? 0 : 1;
 			if(buffer[n].min > kmers.km[strand]){
-				buffer[n].min    = kmers.km[strand];
+				buffer[n].min  = kmers.km[strand];
 				buffer[n].load = (uint64_t)rid<<32 | (uint32_t)i<<1 | strand;
 			}
+			j = 0;
+			l = i + w;
+			n += 1;
 		}
 	}
-	printf("seq: %s min: %i\n", name, n);
-	for(i = 0; i < 10; i++){
-		print_mr(&buffer[i]);
+	printf("seq: %s has %i minimizers\n", name, n );
+
+	*data = (struct mr *) realloc(*data, (n + (*datumSize)) * sizeof(struct mr));
+
+	for(i = 0; i < n; i++){
+		(*data)[i + (*datumSize)].load = buffer[i].load;
+		(*data)[i + (*datumSize)].min  = buffer[i].min;
 	}
-	for(i = n -1; i > n-10; i--){
-		print_mr(&buffer[i]);
-	}
+	*datumSize += n;
 }
 
 KSEQ_INIT(gzFile, gzread)
 
 int main(int argc, char *argv[])
 {
+	int nMinimizers = 0;
+	struct mr * kmers_and_positions = 0;
+	kmers_and_positions = (struct mr*)malloc(sizeof(struct mr));
+
   gzFile fp;
   kseq_t *seq;
   int l;
@@ -139,7 +150,7 @@ int main(int argc, char *argv[])
 	uint32_t rid = 0;
 
   while ((l = kseq_read(seq)) >= 0) { // STEP 4: read sequence
-		sketch(seq->name.s, seq->seq.s, seq->seq.l, 10, 5, rid);
+		sketch(seq->name.s, seq->seq.s, seq->seq.l, 10, 10, rid, &kmers_and_positions, &nMinimizers);
 		rid++;
 	}
   kseq_destroy(seq); // STEP 5: destroy seq
